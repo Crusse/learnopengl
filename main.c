0e6b3d95d3ca90ffa8e6b73b91157da4438c7be8
Tony Martin <tony.martin@astehelsinki.fi>
2017-07-12 19:25:28 +0300

    Shader tests

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <glad/glad.h>
 #include <SDL2/SDL.h>
 
@@ -10,6 +11,79 @@
 #define INITIAL_WIN_H 480
 #define MAX_GL_INFO_LOG 512
 #define TOOLBAR_H 100
+
+typedef struct vec3 {
+  float x;
+  float y;
+  float z;
+} vec3_t;
+
+typedef struct shader {
+  unsigned programId;
+} shader_t;
+
+typedef struct game_object {
+  unsigned vao;
+  unsigned vertex_count;
+} game_object_t;
+
+SDL_Window *window;
+SDL_GLContext *glContext;
+
+int r_init() {
+
+  if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+    fprintf( stderr, "Could not init SDL: %s\n", SDL_GetError() );
+    return 0;
+  }
+
+  SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
+  SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
+  SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+
+  window = SDL_CreateWindow(
+    "OpenGL test",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    INITIAL_WIN_W,
+    INITIAL_WIN_H,
+    SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+  );
+
+  if ( window == NULL ) {
+    fprintf( stderr, "Could not create window: %s\n", SDL_GetError() );
+    return 0;
+  }
+
+  glContext = SDL_GL_CreateContext( window );
+
+  if ( !gladLoadGLLoader( (GLADloadproc) SDL_GL_GetProcAddress ) ) {
+    fprintf( stderr, "Could not init GLAD\n" );
+    return 0;
+  }
+
+  // Vsync
+  SDL_GL_SetSwapInterval( 1 );
+  glClearColor( 0.0f, 0.5f, 0.0f, 1.0f );
+  /*
+   * "Behind the scenes OpenGL uses the data specified via glViewport to
+   * transform the 2D coordinates it processed to coordinates on your screen.
+   * For example, a processed point of location (-0.5,0.5) would (as its final
+   * transformation) be mapped to (200,450) in screen coordinates. Note that
+   * processed coordinates in OpenGL are between -1 and 1 so we effectively map
+   * from the range (-1 to 1) to (0, 800) and (0, 600)."
+   */
+  glViewport( 0, TOOLBAR_H, INITIAL_WIN_W, INITIAL_WIN_H - TOOLBAR_H );
+
+  return 1;
+}
+
+void r_destroy() {
+
+  SDL_GL_DeleteContext( glContext );
+  SDL_DestroyWindow( window );
+  SDL_Quit();
+}
 
 void game_tick( double dt ) {
 
@@ -37,18 +111,20 @@ const char* get_file_contents( const char *path ) {
 
   fclose( file );
 
-  i = '\0';
+  *i = '\0';
 
   return ret;
 }
 
-unsigned compile_shader( char *filePath, GLenum shaderType ) {
+unsigned compile_shader( const char *filePath, GLenum shaderType ) {
 
   unsigned shader = glCreateShader( shaderType );
   const char * const shaderStr = get_file_contents( filePath );
 
-  if ( !shaderStr )
+  if ( !shaderStr ) {
+    fprintf( stderr, "File not found: %s\n", filePath );
     return 0;
+  }
 
   glShaderSource( shader, 1, &shaderStr, NULL );
 
@@ -60,19 +136,22 @@ unsigned compile_shader( char *filePath, GLenum shaderType ) {
 
   if ( !success ) {
     glGetShaderInfoLog( shader, MAX_GL_INFO_LOG, NULL, infoLog );
-    fprintf( stderr, "Shader compilation failed: %s\n", infoLog );
+    fprintf( stderr, "Shader %s compilation failed: %s\n", filePath, infoLog );
     return 0;
   }
 
   return shader;
 }
 
-unsigned create_shader_program( unsigned *shaders, int numShaders ) {
+shader_t* create_shader( const char *vertexShaderPath, const char *fragShaderPath ) {
 
   unsigned shaderProgram = glCreateProgram();
 
-  for ( int i = 0; i < numShaders; ++i )
-    glAttachShader( shaderProgram, shaders[ i ] );
+  unsigned vertexShader = compile_shader( vertexShaderPath, GL_VERTEX_SHADER );
+  glAttachShader( shaderProgram, vertexShader );
+
+  unsigned fragShader = compile_shader( fragShaderPath, GL_FRAGMENT_SHADER );
+  glAttachShader( shaderProgram, fragShader );
 
   int success = 0;
   char infoLog[ MAX_GL_INFO_LOG ] = { 0 };
@@ -80,104 +159,116 @@ unsigned create_shader_program( unsigned *shaders, int numShaders ) {
   glLinkProgram( shaderProgram );
   glGetProgramiv( shaderProgram, GL_LINK_STATUS, &success );
 
+  glDeleteShader( vertexShader );
+  glDeleteShader( fragShader );
+
   if ( !success ) {
     glGetProgramInfoLog( shaderProgram, MAX_GL_INFO_LOG, NULL, infoLog );
     fprintf( stderr, "Shader program linking failed: %s\n", infoLog );
     return 0;
   }
 
-  return shaderProgram;
+  shader_t *ret = malloc( sizeof( shader_t ) );
+  ret->programId = shaderProgram;
+
+  return ret;
 }
 
-int main() {
+void activate_shader( const shader_t *shader ) {
+  glUseProgram( shader->programId );
+}
 
-  int quit = 0;
-  SDL_Event event;
-
-  if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-    fprintf( stderr, "Could not init SDL: %s\n", SDL_GetError() );
-    return 1;
-  }
-
-  SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
-  SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
-  SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
-
-  SDL_Window *window = SDL_CreateWindow(
-    "OpenGL test",
-    SDL_WINDOWPOS_CENTERED,
-    SDL_WINDOWPOS_CENTERED,
-    INITIAL_WIN_W,
-    INITIAL_WIN_H,
-    SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
-  );
-
-  if ( window == NULL ) {
-    fprintf( stderr, "Could not create window: %s\n", SDL_GetError() );
-    return 1;
-  }
-
-  SDL_GLContext *glCtx = SDL_GL_CreateContext( window );
-
-  if ( !gladLoadGLLoader( (GLADloadproc) SDL_GL_GetProcAddress ) ) {
-    fprintf( stderr, "Could not init GLAD\n" );
-    return 1;
-  }
-
-  // Vsync
-  SDL_GL_SetSwapInterval( 1 );
-  glClearColor( 0.0f, 0.5f, 0.0f, 1.0f );
-  /*
-   * "Behind the scenes OpenGL uses the data specified via glViewport to
-   * transform the 2D coordinates it processed to coordinates on your screen.
-   * For example, a processed point of location (-0.5,0.5) would (as its final
-   * transformation) be mapped to (200,450) in screen coordinates. Note that
-   * processed coordinates in OpenGL are between -1 and 1 so we effectively map
-   * from the range (-1 to 1) to (0, 800) and (0, 600)."
-   */
-  glViewport( 0, TOOLBAR_H, INITIAL_WIN_W, INITIAL_WIN_H );
-
-  // ----------------------------------------------------
+game_object_t* create_object( const vec3_t *locations, const vec3_t *colors, unsigned nVertices ) {
 
   unsigned vao;
   glGenVertexArrays( 1, &vao );
   glBindVertexArray( vao );
 
-  /*
-    "Once your vertex coordinates have been processed in the vertex shader,
-    they should be in normalized device coordinates which is a small space
-    where the x, y and z values vary from -1.0 to 1.0."
-   */
-  float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.0f, 0.5f, 0.0f,
-     0.5f,  -0.5f, 0.0f
-  };
   unsigned vbo;
+  vec3_t vertices[ nVertices * 2 ]; // VLA
+
+  for ( unsigned i = 0; i < nVertices; ++i ) {
+    vertices[ i ] = locations[ i ];
+    vertices[ nVertices + i ] = colors[ i ];
+  }
+
   glGenBuffers( 1, &vbo );
   // "OpenGL has many types of buffer objects and the buffer type of a vertex
   // buffer object is GL_ARRAY_BUFFER. OpenGL allows us to bind to several
   // buffers at once as long as they have a different buffer type."
   glBindBuffer( GL_ARRAY_BUFFER, vbo );
-  glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+  // This apparently transfers data to GPU memory.
+  // https://cognitivewaves.wordpress.com/opengl-terminology-demystified/
+  glBufferData( GL_ARRAY_BUFFER, nVertices * 2 * sizeof( vec3_t ), vertices, GL_STATIC_DRAW );
 
-  unsigned vertexShader = compile_shader( "shader.vert", GL_VERTEX_SHADER );
-  unsigned fragShader = compile_shader( "shader.frag", GL_FRAGMENT_SHADER );
-  unsigned shaders[] = { vertexShader, fragShader };
-  unsigned shaderProgram = create_shader_program( shaders, 2 );
-
-  glDeleteShader( vertexShader );
-  glDeleteShader( fragShader );
-
-  // "The vertex attribute is a vec3 so it is composed of 3 values."
   // "...the position vertex attribute in the vertex shader with layout (location = 0). This sets the location of the vertex attribute to 0..."
+  // "The vertex attribute is a vec3 so it is composed of 3 values."
   // "The third argument specifies the type of the data which is GL_FLOAT (a vec* in GLSL consists of floating point values)."
   // "The next argument specifies if we want the data to be normalized."
   // "The fifth argument is known as the stride and tells us the space between consecutive vertex attribute sets."
   // "The last parameter is of type void* and thus requires that weird cast. This is the offset of where the position data begins in the buffer."
   glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( float ), (void*) 0 );
   glEnableVertexAttribArray( 0 );
-  glUseProgram( shaderProgram );
+
+  glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( float ), (void*) ( nVertices * sizeof( vec3_t ) ) );
+  glEnableVertexAttribArray( 1 );
+
+  glBindVertexArray( 0 );
+  glBindBuffer( GL_ARRAY_BUFFER, 0 );
+
+  game_object_t *obj = malloc( sizeof( game_object_t ) );
+  if ( !obj )
+    return NULL;
+
+  obj->vao = vao;
+  obj->vertex_count = nVertices;
+
+  return obj;
+}
+
+void render_object( const game_object_t *obj ) {
+
+  glBindVertexArray( obj->vao );
+  glDrawArrays( GL_TRIANGLES, 0, obj->vertex_count );
+  glBindVertexArray( 0 );
+}
+
+int main() {
+
+  if ( !r_init() )
+    return 1;
+
+  // ----------------------------------------------------
+
+  game_object_t *obj1 = create_object(
+    (vec3_t[]) { 
+      { -1.0f, -1.0f, 0.0f },
+      { -0.5f, 0.0f, 0.0f },
+      { 0.0f,  -1.0f, 0.0f }
+    },
+    (vec3_t[]) { 
+      { 1.0f, 0.0f, 0.0f },
+      { 0.0f, 1.0f, 0.0f },
+      { 0.0f, 0.0f, 1.0f }
+    },
+    3
+  );
+  game_object_t *obj2 = create_object(
+    (vec3_t[]) {
+      { 0.0f, 0.0f, 0.0f },
+      { 0.5f, 1.0f, 0.0f },
+      { 1.0f,  0.0f, 0.0f }
+    },
+    (vec3_t[]) { 
+      { 1.0f, 1.0f, 1.0f },
+      { 0.666f, 0.666f, 0.666f },
+      { 0.333f, 0.333f, 0.333f }
+    },
+    3
+  );
+
+  shader_t *shader1 = create_shader( "shader.vert",  "shader.frag" );
+  shader_t *shader2 = create_shader( "shader.vert",  "shader2.frag" );
 
   // ----------------------------------------------------
 
@@ -186,6 +277,8 @@ int main() {
   Uint32 gameTicks = 0;
   unsigned frames = 0;
   Uint32 fpsStart = lastUpdate;
+  SDL_Event event;
+  int quit = 0;
 
   printf( "MS_PER_TICK: %f\n", MS_PER_TICK );
 
@@ -204,7 +297,7 @@ int main() {
         switch ( event.window.event ) {
           case SDL_WINDOWEVENT_RESIZED:
           case SDL_WINDOWEVENT_SIZE_CHANGED:
-            glViewport( 0, TOOLBAR_H, event.window.data1, event.window.data2 );
+            glViewport( 0, TOOLBAR_H, event.window.data1, event.window.data2 - TOOLBAR_H );
             printf( "Window size: %d x %d\n", event.window.data1, event.window.data2 );
             break;
         }
@@ -223,9 +316,18 @@ int main() {
     // ----------------------------------------------------
 
     glClear( GL_COLOR_BUFFER_BIT );
-    glBindVertexArray( vao );
-    glDrawArrays( GL_TRIANGLES, 0, 3 );
-    glBindVertexArray( 0 );
+
+    float lightness = sinf( now / 1000.0f ) / 2.0f + 0.5f;
+    int lightnessLocation = glGetUniformLocation( shader1->programId, "lightness" );
+
+    activate_shader( shader1 );
+    glUniform1f( lightnessLocation, lightness );
+    render_object( obj1 );
+
+    activate_shader( shader2 );
+    lightnessLocation = glGetUniformLocation( shader2->programId, "lightness" );
+    glUniform1f( lightnessLocation, 0.5 );
+    render_object( obj2 );
 
     // FIXME: "On Mac OS X make sure you bind 0 to the draw framebuffer before
     // swapping the window, otherwise nothing will happen"
@@ -245,9 +347,7 @@ int main() {
     lastUpdate = now;
   }
 
-  SDL_GL_DeleteContext( glCtx );
-  SDL_DestroyWindow( window );
-  SDL_Quit();
+  r_destroy();
 
   return 0;
 }
